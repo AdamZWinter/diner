e58a8f5ccd8be4319106430f8cac3486a77f2efe
AdamZWinter <adam@bootstrapmusic.com>
2023-01-24 18:12:30 -0500

    order form submits to self and redirects to summary

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Start a session
+session_start();
+
 //Require autoload file
 require_once('vendor/autoload.php');
 
@@ -33,6 +36,27 @@ $f3->route('GET /lunch', function (){
     //Instantiate a view
     $view = new Template();
     echo $view->render("views/lunch.html");
+});
+
+//Define a route to order form 1
+$f3->route('GET|POST /order1', function () use ($f3) {
+    if ($_SERVER['REQUEST_METHOD'] ==  'POST'){
+        //Move data from POST array to SESSION array
+        $_SESSION['meal'] = $_POST['meal'];
+        $_SESSION['fname'] = $_POST['fname'];
+        $f3->reroute('summary');
+
+    }
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/order-form1.html");
+});
+
+//Define a route to summary
+$f3->route('GET /summary', function (){
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/order-summary.html");
 });
 
 
